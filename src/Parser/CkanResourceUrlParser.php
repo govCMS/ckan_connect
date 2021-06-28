7325facb162eebea772c92da1059e46ca6f294af
Wilson Wu <wwu@doghouse.agency>
2021-06-28 15:59:58 +1000

    #DVFD9-47 modify the logic for parse() function using preg_match() to get the resource_id from the url

--- src/Parser/CkanResourceUrlParser.php
+++ src/Parser/CkanResourceUrlParser.php
@@ -15,6 +15,7 @@ class CkanResourceUrlParser implements CkanResourceUrlParserInterface {
    * Regex to check whether a CKAN resource path is valid.
    */
   const CKAN_RESOURCE_PATH_REGEX = '/(?:resource\/|resource_id=)([a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12})/';
+  const CKAN_PACKAGE_PATH_REGEX = '/(?:dataset\/|package_id=)([a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12})/';
 
   /**
    * The CKAN client.
@@ -47,36 +48,18 @@ class CkanResourceUrlParser implements CkanResourceUrlParserInterface {
    * {@inheritdoc}
    */
   public function parse($url) {
-    if (!preg_match(self::CKAN_RESOURCE_PATH_REGEX, $url)) {
-      return FALSE;
-    }
-
-    /**
-     * There are three types of url
-     * /dataset/.../resource/RESOURCE_ID
-     * /dataset/PACKAGE_ID/resource/RESOURCE_ID/download/...
-     * /data/api/.../action/datastore_search?resource_id=RESOURCE_ID...
-     */
-    $options = parse_url($url);
-    if(!empty($options['query'])) {
-      parse_str($options['query'], $query_string);
-    }
-
-    // Get the resource_id from the query string or parse the url to get the resource_id.
-    if(!empty($query_string['resource_id'])){
-      $options = [
-        'resource_id' => $query_string['resource_id']
-      ];
-    } else {
-      $parts = explode('/', $options['path']);
-      $index = array_search('resource', $parts);
-      $options = [
-        'resource_id' => $parts[$index + 1]
-      ];
-      if(in_array("download", $parts)) {
-        $options['package_id'] = $parts[2];
+    if (!preg_match(self::CKAN_RESOURCE_PATH_REGEX, $url, $matches)) {
+      if (empty($matches[1])) {
+        return FALSE;
       }
     }
+    $options['resource_id'] = $matches[1];
+
+    preg_match(self::CKAN_PACKAGE_PATH_REGEX, $url, $matches);
+    if (!empty($matches[1])) {
+      $options['package_id'] = $matches[1];
+    }
+
     return $options;
   }
 
