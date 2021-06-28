09866dae07d353c26516cbffed56a06d344c1acd
Wilson Wu <wwu@doghouse.agency>
2021-06-28 14:41:32 +1000

    #DVFD9-47 URL format of CKAN resource parser needs to match URL provided by data.gov.au

--- src/Parser/CkanResourceUrlParser.php
+++ src/Parser/CkanResourceUrlParser.php
@@ -14,7 +14,7 @@ class CkanResourceUrlParser implements CkanResourceUrlParserInterface {
   /**
    * Regex to check whether a CKAN resource path is valid.
    */
-  const CKAN_RESOURCE_PATH_REGEX = '/^\/dataset\/.*\/resource\/[^\/]+$/';
+  const CKAN_RESOURCE_PATH_REGEX = '/(?:resource\/|resource_id=)([a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12})/';
 
   /**
    * The CKAN client.
@@ -47,20 +47,36 @@ class CkanResourceUrlParser implements CkanResourceUrlParserInterface {
    * {@inheritdoc}
    */
   public function parse($url) {
-    $options = parse_url($url);
-    $path = !empty($options['path']) ? $options['path'] : '';
-
-    if (!preg_match(self::CKAN_RESOURCE_PATH_REGEX, $path)) {
+    if (!preg_match(self::CKAN_RESOURCE_PATH_REGEX, $url)) {
       return FALSE;
     }
 
-    // Expected format /dataset/PACKAGE_ID/resource/RESOURCE_ID.
-    $parts = explode('/', $options['path'], 5);
-    $options = [
-      'package_id' => $parts[2],
-      'resource_id' => $parts[4],
-    ];
+    /**
+     * There are three types of url
+     * /dataset/.../resource/RESOURCE_ID
+     * /dataset/PACKAGE_ID/resource/RESOURCE_ID/download/...
+     * /data/api/.../action/datastore_search?resource_id=RESOURCE_ID...
+     */
+    $options = parse_url($url);
+    if(!empty($options['query'])) {
+      parse_str($options['query'], $query_string);
+    }
 
+    //Get the resource_id from the query string or parse the url to get the resource_id
+    if(!empty($query_string['resource_id'])){
+      $options = [
+        'resource_id' => $query_string['resource_id']
+      ];
+    } else {
+      $parts = explode('/', $options['path']);
+      $index = array_search('resource', $parts);
+      $options = [
+        'resource_id' => $parts[$index + 1]
+      ];
+      if(in_array("download", $parts)) {
+        $options['package_id'] = $parts[2];
+      }
+    }
     return $options;
   }
 
